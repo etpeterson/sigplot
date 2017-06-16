a493bcb9d2021189abed0be36529119fbd281ad2
Eric Peterson <etpeterson@users.noreply.github.com>
2017-06-16 12:14:22 -0700

    Up/down arrows automatically resize

--- sigplot.R
+++ sigplot.R
@@ -32,7 +32,9 @@ sigplot <- function(fitdf){
   fitmelt <- melt(fitmat, varnames = c("Region","Variable"))
   fitmelt <- cbind(fitmelt, fnum2str(fitmelt$value))
   if (hascoef){
-    fitmelt <- cbind(fitmelt, est=sign(fitmeltcoef$value))
+    est <- sign(fitmeltcoef$value)
+    est[is.na(est)] <- 0
+    fitmelt <- cbind(fitmelt, est=est)
   }
   lims <- c(0, 0.2)
   breakvec <- c(0, 0.01, 0.05, 0.075, 0.1, 0.15, 0.2)
@@ -45,7 +47,9 @@ sigplot <- function(fitdf){
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), legend.key.height = unit(0.1, "npc"), legend.background = element_rect(fill = "gray95")) +
     coord_fixed()
   if (hascoef){
-    gcp <- gcp + geom_segment(aes(x=as.numeric(Variable),xend=as.numeric(Variable),y=as.numeric(Region)+est*0.25,yend=as.numeric(Region)+est*0.45),arrow=arrow(type="closed",length=unit(0.03,"npc"))) #0.03 or 0.1, why changing?
+    #gcp <- gcp + geom_segment(aes(x=as.numeric(Variable),xend=as.numeric(Variable),y=as.numeric(Region)+est*0.25,yend=as.numeric(Region)+est*0.45),arrow=arrow(type="closed",length=unit(0.1,"native"))) #0.03 or 0.1, why changing?
+    positions <- data.frame(id=rep(seq(nrow(fitmelt)), each=3), x_tri=c(rbind(as.numeric(fitmelt$Variable)-0.15,as.numeric(fitmelt$Variable),as.numeric(fitmelt$Variable)+0.15)), y_tri=c(rbind(as.numeric(fitmelt$Region)+fitmelt$est*0.2,yend=as.numeric(fitmelt$Region)+fitmelt$est*0.45,as.numeric(fitmelt$Region)+fitmelt$est*0.2)), value_black=rep(1, nrow(fitmelt)))
+    gcp <- gcp + geom_polygon(data=positions, aes(x=x_tri, y=y_tri, group=id, fill=value_black))
   }
   gcp <- gcp + geom_text(aes(x=Variable, y=Region, label = value_str), color = "red", size = 3) #color = "red"
   #gcp <- gcp + geom_text(aes(x=Variable, y=Region, label = sprintf("%0.2f", round(value, digits = 2))), color = "red", size = 3) #color = "red"
