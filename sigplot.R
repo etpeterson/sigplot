0555b595d4d940a1b0c12bc8ede5a2f43b179b33
Eric Peterson <etpeterson@users.noreply.github.com>
2017-07-21 11:45:31 -0700

    Added custom region and variable naming and region dropping

--- sigplot.R
+++ sigplot.R
@@ -1,7 +1,7 @@
 #ETP 20170412
 #this function accepts a list of fits and generates a visual matrix of significance
-library(corrplot)
-library(ggcorrplot)
+#library(corrplot)
+#library(ggcorrplot)
 library(plyr)
 library(reshape2)
 
@@ -18,19 +18,38 @@ sigframe <- function(fitlist, coef = "Pr(>|t|)"){  #coef = "Estimate" gives the 
   return(fitdf)
 }
 
-sigplot <- function(fitdf){
+sigplot <- function(fitdf, pthresh=0.05, alphasort=TRUE, region_labels=NULL, variable_labels=NULL, drop_variables=NULL){
   if (class(fitdf) == "list"){
     hascoef <- TRUE
     fitcoef <- sigframe(fitdf, "Estimate")
     fitdf <- sigframe(fitdf)
     fitmatcoef <- as.matrix(fitcoef)
     fitmeltcoef <- melt(fitmatcoef, varnames = c("Region","Variable"))
+    if (!is.null(drop_variables)){
+      fitmeltcoef <- fitmeltcoef[-grep(paste(drop_variables,collapse="|"),fitmeltcoef$Variable,ignore.case=TRUE),]
+    }
   }else{
     hascoef <- FALSE
   }
   fitmat <- as.matrix(fitdf)
   fitmelt <- melt(fitmat, varnames = c("Region","Variable"))
-  fitmelt <- cbind(fitmelt, fnum2str(fitmelt$value))
+  if (!is.null(drop_variables)){
+    fitmelt <- fitmelt[-grep(paste(drop_variables,collapse="|"),fitmelt$Variable,ignore.case=TRUE),]
+  }
+  if (alphasort){
+    if (is.null(region_labels)){
+      fitmelt$Region <- factor(fitmelt$Region, levels=sort(levels(fitmelt$Region), decreasing=TRUE))
+    }else{
+      fitmelt$Region <- factor(fitmelt$Region, levels=names(sort(region_labels, decreasing=TRUE)))
+    }
+    if (is.null(variable_labels)){
+      fitmelt$Variable <- factor(fitmelt$Variable, levels=sort(levels(fitmelt$Variable)))
+    }else{
+      fitmelt$Variable <- factor(fitmelt$Variable, levels=names(sort(variable_labels)))
+    }
+    
+    fitmelt <- cbind(fitmelt, fnum2str(fitmelt$value, c=pthresh))
+  }
   if (hascoef){
     est <- sign(fitmeltcoef$value)
     est[is.na(est)] <- 0
@@ -46,6 +65,12 @@ sigplot <- function(fitdf){
     scale_fill_gradientn(colors = cvec, breaks = breakvec, values = breakvecnorm, limits = lims, name = "p-value", na.value = "black") +
     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1), legend.key.height = unit(0.1, "npc"), legend.background = element_rect(fill = "gray95")) +
     coord_fixed()
+  if (!is.null(region_labels)){
+    gcp <- gcp + scale_y_discrete(labels=region_labels)
+  }
+  if (!is.null(variable_labels)){
+    gcp <- gcp + scale_x_discrete(labels=variable_labels)
+  }
   if (hascoef){
     #gcp <- gcp + geom_segment(aes(x=as.numeric(Variable),xend=as.numeric(Variable),y=as.numeric(Region)+est*0.25,yend=as.numeric(Region)+est*0.45),arrow=arrow(type="closed",length=unit(0.1,"native"))) #0.03 or 0.1, why changing?
     positions <- data.frame(id=rep(seq(nrow(fitmelt)), each=3), x_tri=c(rbind(as.numeric(fitmelt$Variable)-0.15,as.numeric(fitmelt$Variable),as.numeric(fitmelt$Variable)+0.15)), y_tri=c(rbind(as.numeric(fitmelt$Region)+fitmelt$est*0.2,yend=as.numeric(fitmelt$Region)+fitmelt$est*0.45,as.numeric(fitmelt$Region)+fitmelt$est*0.2)), value_black=rep(1, nrow(fitmelt)))
